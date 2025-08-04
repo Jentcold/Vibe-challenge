b2bbe04976f8f57e8acb3c3e057b73086b6df5ac
Mostafa baghdady <152034163+Jentcold@users.noreply.github.com>
2025-08-05 00:38:52 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { SectionsModule } from './sections/sections.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/vibe-challenge'),
+    MongooseModule.forRoot('mongodb+srv://Vibe_challenge:${process.env.DB_PASSWORD}@vibe.ybz7omx.mongodb.net/?retryWrites=true&w=majority&appName=Vibe'),
     SectionsModule,
   ],
 })
